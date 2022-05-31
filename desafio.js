cbada9f6154838b18d82f88be4602c8c13417953
henry <enriqueafrutos@gmail.com>
2022-05-31 16:56:24 -0300

    first commit

---
+++ desafio.js
@@ -0,0 +1,24 @@
+//sumar dos numeros, aumentando el segundo numero en uno hasta que llega a 40
+
+/*let numero = parseInt(prompt('ingresa un numero'));
+let numero2= parseInt(prompt('ingresa el segundo numero'));
+for(numero2; numero2 <= 40; numero2++){
+    console.log(numero+ numero2)
+}
+*/
+
+//segundo ejemplo
+
+/*let texto = prompt('ingresa tu nombre o ESC para salir')
+while (texto != 'ESC' && texto!='esc'){
+    alert('tu nombre es ' + texto)
+    texto = prompt('ingresa tu nombre o ESC para salir')
+}*/
+
+//tercer ejemplo
+
+let numero = parseInt(prompt('ingresa la cantidad de holas'))
+for (let i=1;i<= numero;i++){
+    alert('hola');
+}
+
